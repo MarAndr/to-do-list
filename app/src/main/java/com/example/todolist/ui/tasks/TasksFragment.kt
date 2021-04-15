f63b9092e47447d77cc7ba17f876cfb9b75c5b24
andrey marin <anmarin86@gmail.com>
2021-04-15 17:50:15 +0300

    add recyclerView

--- app/src/main/java/com/example/todolist/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/example/todolist/ui/tasks/TasksFragment.kt
@@ -1,7 +1,10 @@
 package com.example.todolist.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ViewBindingFragment
 import com.example.todolist.R
 import com.example.todolist.databinding.FragmentTasksBinding
@@ -11,4 +14,24 @@ import dagger.hilt.android.AndroidEntryPoint
 class TasksFragment: ViewBindingFragment<FragmentTasksBinding>(FragmentTasksBinding::inflate) {
 
     private val viewModel: TasksViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val tasksAdapter = TasksAdapter()
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = tasksAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+        viewModel.tasks.observe(viewLifecycleOwner){
+            tasksAdapter.submitList(it)
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/todolist/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/example/todolist/ui/tasks/TasksViewModel.kt
@@ -2,9 +2,12 @@ package com.example.todolist.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.example.todolist.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ): ViewModel() {
+
+    val tasks = taskDao.getTasks().asLiveData()
 }
