47da2a33b7b13551c0afcc4379c06286403ff9c8
andrey marin <anmarin86@gmail.com>
2021-04-15 13:53:22 +0300

    add basic layouts and set navigation

---
+++ app/src/main/java/com/example/todolist/ui/tasks/TasksFragment.kt
@@ -0,0 +1,10 @@
+package com.example.todolist.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.example.ViewBindingFragment
+import com.example.todolist.R
+import com.example.todolist.databinding.FragmentTasksBinding
+
+class TasksFragment: ViewBindingFragment<FragmentTasksBinding>(FragmentTasksBinding::inflate) {
+
+}
