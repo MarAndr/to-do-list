0c059e7aca94f59801e4a95a62b7e5fe216e8a6f
andrey marin <anmarin86@gmail.com>
2021-04-16 08:33:00 +0300

    add menu with search in tasksFragment

--- app/src/main/java/com/example/todolist/data/TaskDao.kt
+++ app/src/main/java/com/example/todolist/data/TaskDao.kt
@@ -6,8 +6,8 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TaskDao{
-    @Query("SELECT * FROM ${TasksContract.TABLE_NAME}")
-    fun getTasks(): Flow<List<Task>>
+    @Query("SELECT * FROM ${TasksContract.TABLE_NAME} WHERE ${TasksContract.Columns.NAME} LIKE '%' || :searchQuery || '%' ORDER BY ${TasksContract.Columns.IMPORTANT} DESC")
+    fun getTasks(searchQuery: String): Flow<List<Task>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTask(task: Task)
